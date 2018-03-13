1f99b58597f2d3ba9e7a115c36412657f457a8ee
Dina <dina.nashaat@gmail.com>
2018-03-13 17:21:07 +0200

    initial working version

--- Connection.py
+++ Connection.py
@@ -2,16 +2,33 @@ import threading
 import socket
 
 
+
+
 class Connection(threading.Thread):
+
     def __init__(self, host, port):
         super(Connection, self).__init__()
+        self.conn = None
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
         self.sock.listen(3)
+        print("The server has started and listening")
+        print(port)
 
-    
+
+    def connect(self):
+        print("Listening:")
+        self.conn, self.addr = self.sock.accept()
+        REQUEST_TYPE = self.conn.recv(1024)
+        self.conn.sendall('0')
+        print(REQUEST_TYPE)
+        return REQUEST_TYPE
+
     def handleThread(self, request_type):
-        if(request_type == 0):
+        if(request_type == '0'):
             print("Hello Reader")
-        elif (request_type == 1):
+
+        elif (request_type == '1'):
             print("Hello Writer")
+        else:
+            print("Nothing of value")
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from threading import Thread
 from Connection import Connection
 
 #Initial Values
-SERVERIP = '127.0.0.1'
+SERVERIP = '192.168.1.26'
 SERVERPORT = 8888
 THREADS = []
 
@@ -15,13 +15,15 @@ print "Server has Started"
 # Start main server
 REQUEST_TYPE = None
 Nsequence = 0
-while not REQUEST_TYPE:
-    try:
-        REQUEST_TYPE = server.sock.recv()
-        THREAD = server(SERVERIP, randrange(2000, 8000))
-        THREADS.append(THREAD)
-        print "Thread Created"
-        Thread(target=THREAD.handleThread, args=[REQUEST_TYPE]).start()
-        REQUEST_TYPE = None
-    except socket.timeout:
-        pass
+# while not REQUEST_TYPE
+try:
+    print("Hello")
+    REQUEST_TYPE = server.connect()
+    print(REQUEST_TYPE)
+    THREAD = Connection(SERVERIP, randrange(2000, 8000))
+    THREADS.append(THREAD)
+    print "Thread Created"
+    Thread(target=THREAD.handleThread, args=[REQUEST_TYPE]).start()
+    REQUEST_TYPE = None
+except socket.timeout:
+    pass
