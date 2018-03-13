26109b3f8061130dbad3a2e1f95861adc6a9e641
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2018-03-13 19:13:41 +0200

    Add start2.py

---
+++ start2.py
@@ -0,0 +1,31 @@
+import time
+from reader import Reader
+from writer import Writer
+from server import Server
+
+host = '192.168.1.26'
+port = 8888
+
+# server = Server(host, port)
+# server.run()
+
+reader1 = Reader(host, port)
+reader1.connect()
+reader1.read(1)
+
+writer1 = Writer(host, port)
+writer1.connect()
+writer1.write(1, '3')
+
+reader2 = Reader(host, port)
+reader2.connect()
+reader2.read(8)
+
+reader1.close()
+print('reader1 done')
+
+writer1.close()
+print('writer1 done')
+
+print('reader2 done')
+reader2.close()
