e6a517aa91c2047e00851751ba4162db03d93ca8
Dina <dina.nashaat@gmail.com>
2018-03-13 18:08:12 +0200

    fixes

--- Connection.py
+++ Connection.py
@@ -2,13 +2,12 @@ import threading
 import socket
 
 
-
-
 class Connection(threading.Thread):
 
     def __init__(self, host, port):
         super(Connection, self).__init__()
         self.conn = None
+        self.var = '4'
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
         self.sock.listen(3)
@@ -19,16 +18,23 @@ class Connection(threading.Thread):
     def connect(self):
         print("Listening:")
         self.conn, self.addr = self.sock.accept()
-        REQUEST_TYPE = self.conn.recv(1024)
-        self.conn.sendall('0')
-        print(REQUEST_TYPE)
-        return REQUEST_TYPE
+        return self.conn
+        # REQUEST_TYPE = self.conn.recv(1024)
+        # print(REQUEST_TYPE)
+        # return REQUEST_TYPE
 
-    def handleThread(self, request_type):
+    def handleThread(self, request, data, out_queue):
+        request_type = request.recv(1)
+        print("The request is:")
+        print(request_type)
         if(request_type == '0'):
+            request.sendall(data)
             print("Hello Reader")
-
+            out_queue.put(data)
         elif (request_type == '1'):
             print("Hello Writer")
+            data = request.recv(1)
+            print(data)
+            out_queue.put(data)
         else:
             print("Nothing of value")
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 from random import randrange
-from threading import Thread
+import threading
 from Connection import Connection
+import Queue
 
 #Initial Values
 SERVERIP = '192.168.1.26'
@@ -17,12 +18,19 @@ class Server:
 # while not REQUEST_TYPE
 
     def run(self):
+        DATA = '4'
+        my_queue = Queue.Queue()
         while not self.request_type:
             try:
                 self.request_type = self.server.connect()
                 THREAD = Connection(SERVERIP, randrange(2000, 8000))
                 THREADS.append(THREAD)
-                Thread(target=THREAD.handleThread, args=[self.request_type]).start()
+                thread1 = threading.Thread(target=THREAD.handleThread, args=[self.request_type, DATA, my_queue])
+                thread1.start()
+                thread1.join()
+                
+                DATA = my_queue.get()
+                print(DATA)
                 self.request_type = None
             except socket.timeout:
                 pass
