b980c2b3c3f2c2ca2b5a92fe7e72c2dd318e556f
Dina <dina.nashaat@gmail.com>
2018-03-21 13:18:46 +0200

    use system.properties

--- mainstart.py
+++ mainstart.py
@@ -1,16 +1,22 @@
+import json
 import paramiko
 
-server = '172.20.10.9'
-username = 'magdz'
-password = '1234'
-port = 8888
+
+
+
+# JSON_PATH = './system.json'
+system_properties = json.load(open('system.properties'))
+
+hostname = system_properties["server"]["host"]
+username = system_properties["server"]["username"]
+password = system_properties["server"]["password"]
+port     = system_properties["server"]["port"]
 
 cmd_to_execute = 'cd Github/BulletinBoard && python start.py'
 
-
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(server, username=username, password=password, port=port)
+ssh.connect(hostname, username=username, password=password, port=port)
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
 
 for line in iter(lambda: ssh_stdout.readline(2048), ""):
