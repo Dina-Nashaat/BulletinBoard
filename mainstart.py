a6277bd78b082569d74b71ab0cef33b36c77b5ce
Dina <dina.nashaat@gmail.com>
2018-03-21 13:44:26 +0200

    use system.properties

--- mainstart.py
+++ mainstart.py
@@ -2,9 +2,6 @@ import json
 import paramiko
 
 
-
-
-# JSON_PATH = './system.json'
 system_properties = json.load(open('system.properties'))
 
 hostname = system_properties["server"]["host"]
@@ -18,6 +15,19 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname, username=username, password=password, port=port)
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
+
+
+hostname = system_properties["reader0"]["host"]
+username = system_properties["reader0"]["username"]
+password = system_properties["reader0"]["password"]
+port = system_properties["reader0"]["port"]
+
+cmd_to_execute = ' cd /home/dina/Documents/BulletinBoard && python start2.py'
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname, username=username, password=password, port=port)
+ssh_stdin, ssh_stdout2, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
 
 for line in iter(lambda: ssh_stdout.readline(2048), ""):
     print(line)
