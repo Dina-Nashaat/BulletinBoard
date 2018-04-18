7f10ef2d82570f15d96ee2152030dcee3f2a427c
Dina <dina.nashaat@gmail.com>
2018-04-18 16:56:24 +0200

    change host

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from RMIInterface import RMIInterface
 
 class Server:
     def __init__(self):
+        Pyro4.config.HOST = '192.168.8.100'
         daemon = Pyro4.Daemon()
         uri = daemon.register(RMIInterface)
         ns = Pyro4.locateNS()
